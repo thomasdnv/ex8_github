12d21eb013e5d33f154a7e3503c8d8b366d4fb87
thomasdnv <thomasdonovan.bi@gmail.com>
2024-12-16 15:01:27 +0100

    ex8

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Net.NetworkInformation;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        /* EX8 - Which size should I take? */
+
+        
+
+    }
+};
